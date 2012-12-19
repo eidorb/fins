278a714b5b47b6cdf12e6b7d35a0729d15e7d994
Brodie Blackburn <brodie.blackburn@ergon.com.au>
2012-12-19 13:50:48 +1000

    Move trojancomms package

---
+++ trojancomms/fins.py
@@ -0,0 +1,337 @@
+from datetime import datetime
+import logging
+from operator import xor
+import re
+import socket
+
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+# FINS header constants
+RESPONSE_WAIT = '0'
+ICF_COMMAND = '80'
+ICF_RESPONSE = 'C0'
+RSV = '00'
+GCT = '02'
+DNA = '00'
+DA1 = '00'
+DA2 = '00'
+SNA = '00'
+SA1 = '00'
+SA2 = '00'
+SID = '00'
+
+FINS_HEADER = ''.join((RESPONSE_WAIT, ICF_COMMAND, RSV, GCT, DNA, DA1, DA2, SNA,
+                       SA1, SA2, SID))
+ADDRESS_BYTES = ''.join((DNA, DA1, DA2, SNA, SA1, SA2, SID))
+
+# FINS commands
+MEMORY_AREA_READ = '0101'
+MEMORY_AREA_WRITE = '0102'
+CLOCK_READ = '0701'
+CLOCK_WRITE = '0702'
+
+# Memory area codes
+CIO_BIT = '30'
+WR_BIT = '31'
+HR_BIT = '32'
+AR_BIT = '33'
+CIO_WORD = 'B0'
+WR_WORD = 'B1'
+HR_WORD = 'B2'
+AR_WORD = 'B3'
+DM_BIT = '02'
+DM_WORD = '82'
+
+# The size of items are dependent on the memory area. This is a mapping between
+# the memory area and the size of items in bytes returned in the memory read
+# area command.
+ITEM_BYTE_SIZE = {CIO_BIT: 1,
+                  WR_BIT: 1,
+                  HR_BIT: 1,
+                  AR_BIT: 1,
+                  CIO_WORD: 2,
+                  WR_WORD: 2,
+                  HR_WORD: 2,
+                  AR_WORD: 2,
+                  DM_BIT: 1,
+                  DM_WORD: 2}
+
+TCP_TIMEOUT = 2
+
+
+class FINS(object):
+
+    """This class implements various FINS commands.
+
+    The class is instatiated with a connection object that allows transmission
+    of FINS commands over some channel.
+
+    Supported FINS commands are:
+        Memory area read (01 01)
+        Memory area write (01 02)
+        Clock read (07 01)
+        Clock write (07 02)
+    """
+
+    def __init__(self, connection):
+        """`connection` is an object that supports the methods `send(data)` and
+        `receive()`. `send(data)` should return True when data has been
+        successfully sent and `receive()` should return an ASCII FINS response.
+        As long as these methods are supported, it does not matter how the
+        underlying connection is implemented. For example, `connection` may send
+        data using Host Link over serial over TCP/IP.
+        """
+        self.connection = connection
+
+    def send(self, command_code, text='', retries=3):
+        """Construct a FINS command from the specified `command_code` and
+        `text` and send it over the connection. If a valid response is received,
+        it is stripped of FINS headers and verified. `retries` attempts will be
+        made to get a valid response before giving up.
+
+        The text part of the FINS response command is returned.
+        """
+        fins_command = FINS_HEADER + command_code + text
+        attempts = 0
+        while attempts < retries:
+            attempts += 1
+            if self.connection.send(fins_command):
+                response = self.connection.receive()
+                if response is not None:
+                    text = strip_fins_response(response, command_code)
+                    if text is not None:
+                        return text
+        logger.error('Did not receive a valid FINS response command after %s '
+                     'attempts', attempts)
+
+    def memory_area_read(self, memory_area_code, address_word, address_bit=0,
+                         num_items=1, bcd=False):
+        """Returns a list of integers (`num_items` in length) from the
+        specified memory location.
+
+        The memory location is made up of
+            `memory_area_code` - representing an area in memory
+            `address_word` - the address of the first word to read
+            `address_bit` - the first bit in the word to read
+        When reading words, `address_bit` must be set to 0.
+
+        Set `bcd` to True to interpret the response as BCD (binary-coded
+        decimal.)
+
+        `num_items`, `address_word` and `address_bit` must be integers. If
+        `num_items` is 0, an empty list will be returned.
+        """
+        command_text = ''.join((memory_area_code,
+                                hex_string(address_word, bytes=2),
+                                hex_string(address_bit),
+                                hex_string(num_items, bytes=2)))
+        text = self.send(MEMORY_AREA_READ, command_text)
+        if text is not None:
+            # Check that the length of `text` is what we expect. The number of
+            # bytes of each item differs depending on the memory area code.
+            # The byte sizes are specified in `ITEM_BYTE_SIZE`. Each item's
+            # ASCII-encoded bytes are two characters long.
+            item_len = ITEM_BYTE_SIZE[memory_area_code] * 2
+            if num_items * item_len == len(text):
+                logger.debug('Successful memory area read')
+                # Separate items, still strings.
+                items = [text[i:i + item_len]
+                         for i in range(0, len(text), item_len)]
+                base = 10 if bcd is True else 16
+                # Converted to integers.
+                return [int(item, base=base) for item in items]
+
+    def memory_area_read_single(self, memory_area_code, address_word,
+                                address_bit=0, bcd=False):
+        """Simple wrapper around memory_area_read that just returns an single
+        integer.
+        """
+        values = self.memory_area_read(
+            memory_area_code, address_word, address_bit=address_bit,
+            num_items=1, bcd=bcd)
+        try:
+            return values[0]
+        except TypeError:
+            pass
+
+    def memory_area_write(self, memory_area_code, address_word, address_bit=0,
+                          values=[]):
+        """Write integer values from iterable `values` starting at the specified
+        memory location. Returns True on a successful write.
+
+        The memory location is made up of
+            `memory_area_code` - representing an area in memory
+            `address_word` - the address of the first word to read
+            `address_bit` - the first bit in the word to read
+        When writing words, `address_bit` must be set to 0.
+
+        If `values` is simply an integer (e.g. values=0xFF), then
+        `values` will be treated as a single word or bit to be written.
+        """
+        item_bytes = ITEM_BYTE_SIZE[memory_area_code]
+        try:
+            data = ''.join(hex_string(value, bytes=item_bytes)
+                           for value in values)
+            num_items = len(values)
+        except TypeError:
+            data = hex_string(values, bytes=item_bytes)
+            num_items = 1
+        command_text = ''.join((memory_area_code,
+                                hex_string(address_word, bytes=2),
+                                hex_string(address_bit),
+                                hex_string(num_items, bytes=2),
+                                data))
+        text = self.send(MEMORY_AREA_WRITE, command_text)
+        if text == '':
+            return True
+
+    def clock_read(self):
+        """Perform a clock read command and return the response as a datetime
+        object.
+        """
+        text = self.send(CLOCK_READ)
+        if text is not None:
+            try:
+                clock = datetime.strptime(text[:-2], '%y%m%d%H%M%S')
+                logger.debug('Successful clock read')
+                return clock
+            except ValueError:
+                pass
+
+    def clock_write(self, datetime):
+        """Perform a clock write command, setting the clock according to
+        `datetime`. Returns True on successful clock write.
+        """
+        command_text = datetime.strftime('%y%m%d%H%M%S')
+        text = self.send(CLOCK_WRITE, command_text)
+        if text == '':
+            logger.debug('Successful clock write')
+            return True
+
+
+class TCPHostLinkConnection(object):
+
+    """This class supports the sending and receiving of Host Link FINS commands
+    over TCP.
+    """
+
+    def __init__(self, host, port):
+        """Specify a `host` (host name or IP address) and `port` that will
+        accept Host Link commands.
+        """
+        self.host = host
+        self.port = port
+        self.socket = None
+
+    def send(self, fins_command):
+        """Wrap `fins_command` in a Host Link command and send it over a TCP
+        socket.
+
+        Returns True if the FINS command was successfully sent.
+        """
+        hostlink_command = create_hostlink_command(fins_command)
+        try:
+            if self.socket is None:
+                self.socket = socket.create_connection(
+                    (self.host, self.port), TCP_TIMEOUT)
+            self.socket.sendall(hostlink_command)
+            return True
+        except (socket.error, socket.timeout) as e:
+            logger.error('Socket send error: %s', e)
+            if self.socket is not None:
+                self.socket.close()
+            self.socket = None
+
+    def receive(self):
+        """Receive data from a TCP socket and return the underlying FINS
+        response from the Host Link response.
+        """
+        response = ''
+        while '*\r' not in response:
+            try:
+                data = self.socket.recv(4096)
+                if data == '':
+                    logger.error('Socket returned an empty string')
+                    break
+                response += data
+            except (socket.error, socket.timeout) as e:
+                logger.error('Socket receive error: %s', e)
+                break
+        else:
+            return strip_hostlink_response(response)
+        if self.socket is not None:
+            self.socket.close()
+            self.socket = None
+
+
+def hex_string(number, bytes=1):
+    """Return a number converted to its hexadecimal string representation. It
+    will be padded to the specified number of bytes. One byte is two characters.
+    e.g. hex_string(1) returns '01'
+         hex_string(10, 2) returns '000A'
+    """
+    return '{:0{width}X}'.format(number, width=bytes * 2)
+
+
+def calculate_fcs(string):
+    """Return the string representation of an FCS (frame check sequence)
+    computed for the given `string`.
+    """
+    fcs = reduce(xor, (ord(c) for c in string))
+    return hex_string(fcs)
+
+
+def create_hostlink_command(data, device_id='00', header_code='FA'):
+    """Return a Host Link command, encapsulating `data` (hexadecimal ASCII
+    characters).
+
+    `device_id` defaults to '00'; it represents the controller's ID.
+    `header_code` defaults to 'FA' and distinguishes different types of Host
+    Link commands.
+    """
+    header = '@' + device_id + header_code
+    fcs = calculate_fcs(header + data)
+    return header + data + fcs + '*\r'
+
+
+def strip_hostlink_response(response, device_id='00', header_code='FA'):
+    """Strip Host Link header and footer from response. Set `device_id` and
+    `header_code` according to the Host Link command originally sent.
+
+    Return data if everything checks out."""
+    match = re.search(r"""@
+                          {device_id}
+                          {header_code}
+                          (.*)   # Data
+                          (\w\w) # FCS
+                          \*\r   # Terminator
+                      """.format(device_id=device_id, header_code=header_code),
+                      response, re.VERBOSE)
+    if match is not None:
+        data, response_fcs = match.groups()
+        hostlink_response = match.group(0)
+        if calculate_fcs(hostlink_response[:-4]) == response_fcs:
+            return data
+
+
+def strip_fins_response(response, command_code):
+    """Strip FINS header off the response and check that the command code and
+    end code is valid.
+
+    Return FINS text. An empty string ('') will be returned if there is no text.
+    """
+    match = re.match(r"""00
+                         {icf}
+                         \w{{4}}         # Ignore RSV and GCT.
+                         {address_bytes}
+                         {command_code}
+                         0000            # End code
+                         (\w*)           # Data
+                     """.format(icf=ICF_RESPONSE, address_bytes=ADDRESS_BYTES,
+                                command_code=command_code),
+                     response, re.VERBOSE)
+    if match is not None:
+        (text,) = match.groups()
+        return text
---
+++ trojancomms/tests/test_fins.py
@@ -0,0 +1,318 @@
+from datetime import datetime
+import socket
+import unittest
+
+from mock import MagicMock, patch
+
+from trojancomms import fins
+from trojancomms.fins import FINS, TCPHostLinkConnection
+from trojancomms.fins import calculate_fcs, create_hostlink_command, \
+    hex_string, strip_fins_response, strip_hostlink_response
+
+
+class FinsTestCase(unittest.TestCase):
+
+    def test_hex_string(self):
+        self.assertEqual(hex_string(1), '01')
+        self.assertEqual(hex_string(10), '0A')
+        self.assertEqual(hex_string(0xB2), 'B2')
+        self.assertEqual(hex_string(0xB2, 2), '00B2')
+
+    def test_calculate_fcs(self):
+        self.assertEqual(calculate_fcs('@00XZ'), '42')
+        self.assertEqual(
+            calculate_fcs('@00FA08000020000000000000001018203E8000080'), '01')
+        self.assertEqual(
+            calculate_fcs('@00FA0800002000000000000000101B20000000001'), '0C')
+        self.assertEqual(
+            calculate_fcs('@00FA00C0000200000000000000010100000001'), '37')
+        self.assertEqual(
+            calculate_fcs('@00FA0800002000000000000000101B20031000001'), '0E')
+        self.assertEqual(
+            calculate_fcs('@00FA0800002000000000000000101B2002C000001'), '7D')
+        self.assertEqual(
+            calculate_fcs('@00FA00C0000200000000000000010100000000'), '36')
+        self.assertEqual(
+            calculate_fcs('@00FA0800002000000000000000101B20047000001'), '0F')
+        self.assertEqual(
+            calculate_fcs('@00FA00C0000200000000000000010100000503'), '30')
+
+    def test_create_hostlink_command(self):
+        self.assertEqual(
+            '@04RIAL52*\r',
+             create_hostlink_command('AL', device_id='04', header_code='RI'))
+        self.assertEqual(
+             '@00FA00C000020000000000000001010000200733*\r',
+             create_hostlink_command('00C0000200000000000000010100002007'))
+
+    def test_strip_hostlink_response(self):
+        self.assertEqual(
+            '00C0000200000000000000010100002007',
+            strip_hostlink_response(
+                '@00FA00C000020000000000000001010000200733*\r'))
+        self.assertEqual(
+            '00C0000200000000000000010100002037',
+            strip_hostlink_response(
+                '@00FA00C000020000000000000001010000203730*\r'))
+        self.assertEqual(
+            '00C0000200000000000000010100002037',
+            strip_hostlink_response(
+                'XXX@00FA00C000020000000000000001010000203730*\r'))
+        self.assertEqual(
+            '00C0000200000000000000010100002037',
+            strip_hostlink_response(
+                '@00FA00C000020000000000000001010000203730*\rXXX'))
+        self.assertEqual(
+            '00C0000200000000000000010100002037',
+            strip_hostlink_response(
+                'XXX@00FA00C000020000000000000001010000203730*\rXXX'))
+        # FCS error
+        self.assertIsNone(
+            strip_hostlink_response(
+                '@00FA00C000020000000100000001010000203730*\r'))
+
+    def test_strip_fins_response(self):
+        self.assertEqual(
+            '2007',
+            strip_fins_response('00C0000200000000000000010100002007', '0101'))
+        self.assertEqual(
+            '0503',
+            strip_fins_response('00C0010000000000000000010100000503', '0101'))
+        self.assertEqual(
+            '',
+            strip_fins_response('00C000020000000000000001010000', '0101'))
+        self.assertIsNone(
+            strip_fins_response('00C00002000000000000000101000', '0101'))
+        self.assertIsNone(
+            strip_fins_response('008000020000000000000001010000', '0101'))
+        self.assertIsNone(
+            strip_fins_response('00C000020000000000000001010000', '0102'))
+
+    @patch('trojancomms.fins.strip_hostlink_response', return_value='fins')
+    @patch('trojancomms.fins.create_hostlink_command', return_value='hostlink')
+    @patch('trojancomms.fins.socket.create_connection')
+    def test_TCPHostLinkConnection(
+            self, mock_create_connection, mock_create_hostlink_command,
+            mock_strip_hostlink_response):
+        # Test invalid hosts and ports.
+        mock_create_connection.side_effect = socket.gaierror()
+        connection = TCPHostLinkConnection('localhost', 1)
+        self.assertIsNone(connection.send('data'))
+        mock_create_connection.assert_called_with(('localhost', 1), 2)
+
+        # Test sending of data.
+        mock_create_connection.side_effect = None
+        self.assertTrue(connection.send('data'))
+        mock_create_hostlink_command.assert_called_with('data')
+        connection.socket.sendall.assert_called_with('hostlink')
+
+        # Test socket error.
+        mock_socket = MagicMock()
+        mock_socket.sendall.side_effect = socket.error()
+        connection.socket = mock_socket
+        self.assertIsNone(connection.send('data'))
+        self.assertIsNone(connection.socket)
+        mock_socket.close.assert_called_with()
+
+        # Test timeout error.
+        mock_socket.sendall.side_effect = socket.timeout()
+        connection.socket = mock_socket
+        self.assertIsNone(connection.send('data'))
+        self.assertIsNone(connection.socket)
+        mock_socket.close.assert_called_with()
+
+        # Test receive.
+        mock_socket.recv.side_effect = ['test', 'stream*\r']
+        connection.socket = mock_socket
+        self.assertEqual('fins', connection.receive())
+        self.assertIsNotNone(connection.socket)
+        mock_strip_hostlink_response.assert_called_with('teststream*\r')
+
+        mock_socket.recv.side_effect = ['test' 'stream*\r', 'more']
+        connection.socket = mock_socket
+        self.assertEqual('fins', connection.receive())
+        self.assertIsNotNone(connection.socket)
+        mock_strip_hostlink_response.assert_called_with('teststream*\r')
+
+        mock_socket.recv.side_effect = ['test' 'str*\ream', 'more']
+        connection.socket = mock_socket
+        self.assertEqual('fins', connection.receive())
+        self.assertIsNotNone(connection.socket)
+        mock_strip_hostlink_response.assert_called_with('teststr*\ream')
+
+        # Test receive errors.
+        mock_socket.recv.side_effect = ['socket', 'closing', '']
+        connection.socket = mock_socket
+        self.assertIsNone(connection.receive())
+        mock_socket.close.assert_called_with()
+        self.assertIsNone(connection.socket)
+
+        mock_socket.recv.side_effect = ['data', socket.error()]
+        connection.socket = mock_socket
+        self.assertIsNone(connection.receive())
+        mock_socket.close.assert_called_with()
+        self.assertIsNone(connection.socket)
+
+        mock_socket.recv.side_effect = ['data', socket.timeout()]
+        connection.socket = mock_socket
+        self.assertIsNone(connection.receive())
+        mock_socket.close.assert_called_with()
+        self.assertIsNone(connection.socket)
+
+    @patch('trojancomms.fins.strip_fins_response')
+    def test_FINS_send(self, mock_strip_fins_response):
+        connection = MagicMock()
+        fins = FINS(connection)
+        connection.receive.return_value = 'response'
+        mock_strip_fins_response.return_value = 'text'
+
+        # Test sending.
+        self.assertEqual('text', fins.send('FF', '0123'))
+        connection.send.assert_called_once_with('080000200000000000000FF0123')
+        connection.receive.assert_called_once_with()
+        mock_strip_fins_response.assert_called_once_with('response', 'FF')
+
+        # Test unstrippable FINS response.
+        connection.send.reset_mock()
+        connection.receive.reset_mock()
+        mock_strip_fins_response.reset_mock()
+        mock_strip_fins_response.return_value = None
+        self.assertIsNone(fins.send('FF', '0123'))
+        connection.send.assert_called_with('080000200000000000000FF0123')
+        connection.receive.assert_called_with()
+        mock_strip_fins_response.assert_called_with('response', 'FF')
+        self.assertEqual(3, connection.send.call_count)
+        self.assertEqual(3, connection.receive.call_count)
+        self.assertEqual(3, mock_strip_fins_response.call_count)
+
+        # Test no response received.
+        connection.send.reset_mock()
+        connection.receive.reset_mock()
+        mock_strip_fins_response.reset_mock()
+        connection.receive.return_value = None
+        self.assertIsNone(fins.send('FF', '0123'))
+        connection.send.assert_called_with('080000200000000000000FF0123')
+        connection.receive.assert_called_with()
+        self.assertEqual(3, connection.send.call_count)
+        self.assertEqual(3, connection.receive.call_count)
+        self.assertEqual(0, mock_strip_fins_response.call_count)
+
+        # Test can't send.
+        connection.send.reset_mock()
+        connection.receive.reset_mock()
+        mock_strip_fins_response.reset_mock()
+        connection.send.return_value = None
+        self.assertIsNone(fins.send('FF', '0123'))
+        connection.send.assert_called_with('080000200000000000000FF0123')
+        self.assertEqual(3, connection.send.call_count)
+        self.assertEqual(0, connection.receive.call_count)
+        self.assertEqual(0, mock_strip_fins_response.call_count)
+
+    def test_FINS_memory_area_read(self):
+        fins = FINS(None)
+        mock_send = MagicMock()
+        fins.send = mock_send
+
+        # Test send is called with correct parameters.
+        mock_send.return_value = None
+        self.assertIsNone(fins.memory_area_read('B2', 0xBEEF))
+        mock_send.assert_called_with('0101', 'B2BEEF000001')
+        self.assertIsNone(fins.memory_area_read('32', 0xBEEF, 3, num_items=2))
+        mock_send.assert_called_with('0101', '32BEEF030002')
+        fins.memory_area_read('B2', 0xBEEF, num_items=0)
+        mock_send.assert_called_with('0101', 'B2BEEF000000')
+
+        # Test correct parsing of values from response text.
+        mock_send.return_value = 'BEEF'
+        self.assertEqual([0xBEEF], fins.memory_area_read('B2', 0))
+        mock_send.return_value = 'DEADBEEF'
+        self.assertEqual([0xDEAD, 0xBEEF],
+                         fins.memory_area_read('B2', 0, num_items=2))
+
+        # Test that None is returned for more values returned than requested.
+        self.assertIsNone(fins.memory_area_read('B2', 0))
+
+        # Test bit values
+        mock_send.return_value = '000100000101'
+        self.assertEqual([0, 1, 0, 0, 1, 1],
+                         fins.memory_area_read('32', 0, num_items=6))
+
+        # Test BCD.
+        mock_send.return_value = '23456789'
+        self.assertEqual(
+            [2345, 6789], fins.memory_area_read('B2', 0, num_items=2, bcd=True))
+
+    def test_FINS_memory_area_read_single(self):
+        fins = FINS(None)
+        mock_send = MagicMock()
+        fins.send = mock_send
+
+        mock_send.return_value = None
+        self.assertIsNone(fins.memory_area_read_single('B2', 0xBEEF))
+        mock_send.assert_called_with('0101', 'B2BEEF000001')
+
+        mock_send.return_value = 'BEEF'
+        self.assertEqual(0xBEEF, fins.memory_area_read_single('B2', 0))
+
+        # Test bit values
+        mock_send.return_value = '01'
+        self.assertEqual(1, fins.memory_area_read_single('32', 0))
+
+        # Test BCD.
+        mock_send.return_value = '1234'
+        self.assertEqual(
+            1234, fins.memory_area_read_single('B2', 0, bcd=True))
+
+    def test_FINS_memory_area_write(self):
+        fins = FINS(None)
+        mock_send = MagicMock()
+        fins.send = mock_send
+
+        # Test writeing no values.
+        fins.memory_area_write('B2', 0xFACE)
+        mock_send.assert_called_with('0102', 'B2FACE000000')
+
+        # Test writing single value.
+        fins.memory_area_write('B2', 0xBEEF, 0, 0xFACE)
+        mock_send.assert_called_with('0102', 'B2BEEF000001FACE')
+
+        # Test writing multiple values.
+        fins.memory_area_write('B2', 0, values=[0xDEAD, 0xBEEF, 0xFACE])
+        mock_send.assert_called_with('0102', 'B20000000003DEADBEEFFACE')
+        fins.memory_area_write('32', 0, 2, (1, 0, 1, 0, 1, 1, 0, 1))
+        mock_send.assert_called_with('0102', '3200000200080100010001010001')
+
+        # Test unable to send.
+        mock_send.return_value = None
+        self.assertIsNone(fins.memory_area_write('B2', 0xBEEF, 0, 0xFACE))
+
+    def test_FINS_clock_read(self):
+        fins = FINS(None)
+        mock_send = MagicMock()
+        fins.send = mock_send
+
+        # Test correct command code is used.
+        mock_send.return_value = None
+        self.assertIsNone(fins.clock_read())
+        mock_send.assert_called_with('0701')
+
+        # Test response parsing.
+        mock_send.return_value = '12040112345600'
+        self.assertEqual(datetime(2012, 4, 1, 12, 34, 56), fins.clock_read())
+
+        # Test bad date.
+        mock_send.return_value = '12043212345600'
+        self.assertIsNone(fins.clock_read())
+
+    def test_FINS_clock_write(self):
+        fins = FINS(None)
+        mock_send = MagicMock()
+        fins.send = mock_send
+
+        # Test that send is called correctly.
+        fins.clock_write(datetime(2012, 4, 1, 12, 34, 56))
+        mock_send.assert_called_with('0702', '120401123456')
+
+
+if __name__ == '__main__':
+    unittest.main()
